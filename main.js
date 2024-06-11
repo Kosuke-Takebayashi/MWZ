8c7df00b90de4c5bbd45f6fb77d9a27ca584c776
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-06-11 19:25:38 +0900

    ボタンを押すとモーダルを閉じる処理を実装

--- main.js
+++ main.js
@@ -2,16 +2,28 @@ window.onload = function() {
     // モーダルウィンドウの開閉をかく
     // 特定のクラスを付与することでモーダルウィンドウとして機能させる
     const targetImg = document.querySelector('.mwz');
-    const modal = document.createElement('div');
-    const modalImg = document.createElement('img');
-    modal.className = 'mwz-modal';
-    modalImg.className = 'mwz-modal-img';
+    let modalCloseButton;
     
     targetImg.addEventListener('click', function() {
+        const modal = document.createElement('div');
+        const modalImg = document.createElement('img');
+        modalCloseButton = document.createElement('button');
+        modal.className = 'mwz-modal';
+        modalImg.className = 'mwz-modal-img';
+        modalCloseButton.className = 'mwz-modal-close-button';
+        modalCloseButton.type = 'button';
+        modalCloseButton.innerHTML = '閉じる';
         modalImg.src = this.src;
         modal.appendChild(modalImg);
+        modal.appendChild(modalCloseButton);
 
         // モーダルウィンドウを生成
         document.body.insertBefore(modal, null);
+
+        modalCloseButton.addEventListener('click', function() {
+        console.log('OK');
+        modal.remove();
     })
+    })
+
 }
