b1eca7dca5f4570285d6ff8c1be103f310437d13
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-06-11 18:50:03 +0900

    画像クリックでモーダルのタグを生成する処理を実装

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 window.onload = function() {
     // モーダルウィンドウの開閉をかく
     // 特定のクラスを付与することでモーダルウィンドウとして機能させる
-    
+    const targetImg = document.querySelector('.mwz');
+
+    targetImg.addEventListener('click', function() {
+        const el = document.createElement('div');
+
+        document.body.insertBefore(el, null);
+    })
 }
