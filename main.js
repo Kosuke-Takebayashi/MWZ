b0e06bf907b3dff6084880994a48101df507aaf2
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-06-16 01:06:23 +0900

    頓挫

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
 window.onload = function () {
-  // モーダルウィンドウの開閉をかく
-  // 特定のクラスを付与することでモーダルウィンドウとして機能させる
-  const targetImg = document.querySelector(".mwz");
-  let modalCloseButton;
+    const targetImg = document.querySelector(".mwz");
+    let modalCloseButton;
+  
+    targetImg.addEventListener("click", function () {
+      const modal = document.createElement("div");
+      const modalImgWrapper = document.createElement("div");
+      const modalImg = document.createElement("img");
+  
+      modalCloseButton = document.createElement("button");
+      modal.className = "mwz-modal";
+      modalImgWrapper.className = "mwz-modal-img-wrapper";
+      modalImg.className = "mwz-modal-img";
+  
+      modalCloseButton.className = "mwz-modal-close-button";
+      modalCloseButton.type = "button";
+      modalCloseButton.innerHTML = "閉じる";
+  
+      modalImg.src = this.src;
+      modal.appendChild(modalImgWrapper);
+      modalImgWrapper.appendChild(modalImg);
+      modal.appendChild(modalCloseButton);
+  
+      document.body.insertBefore(modal, null);
+  
+      modalImg.addEventListener("mousemove", handleMouseMove);
+  
+      modalCloseButton.addEventListener("click", function () {
+        modal.remove();
+      });
 
-  targetImg.addEventListener("click", function () {
-    const modal = document.createElement("div");
-    const modalImgWrapper = document.createElement("div");
-    const modalImg = document.createElement("img");
-
-    modalCloseButton = document.createElement("button");
-    modal.className = "mwz-modal";
-    modalImgWrapper.className = "mwz-modal-img-wrapper";
-    modalImg.className = "mwz-modal-img";
-
-    modalCloseButton.className = "mwz-modal-close-button";
-    modalCloseButton.type = "button";
-    modalCloseButton.innerHTML = "閉じる";
-    
-    modalImg.src = this.src;
-    modal.appendChild(modalImgWrapper);
-    modalImgWrapper.appendChild(modalImg);
-    modal.appendChild(modalCloseButton);
-
-    // モーダルを生成
-    document.body.insertBefore(modal, null);
-
-    modalImg.addEventListener("mouseover", function () {
-      modalImg.style.scale = "2";
+      modalImg.addEventListener("mouseout", function () {
+        modalImg.style.transform = "translate(0, 0) scale(1)";
+      });
+  
+      function handleMouseMove(event) {
+        const containerRect = modalImgWrapper.getBoundingClientRect();
+        const containerWidth = containerRect.width;
+        const containerHeight = containerRect.height;
+  
+        const imageRect = modalImg.getBoundingClientRect();
+        const imageWidth = imageRect.width;
+        const imageHeight = imageRect.height;
+  
+        const mouseX = event.pageX;
+        const mouseY = event.pageY;
+  
+        const scaleX = 2;
+        const scaleY = 2;
+  
+        const offsetX = (mouseX - containerRect.left - 100) * 1;
+        const offsetY = (mouseY - containerRect.top - 100) * 1;
+  
+        modalImg.style.transformOrigin = `${(mouseX - containerRect.left) / containerWidth * 100}% ${(mouseY - containerRect.top) / containerHeight * 100}%`;
+        modalImg.style.transform = `translate(${offsetX/2}px, ${offsetY/2}px) scale(${scaleX}, ${scaleY})`;
+      }
     });
-
-    modalImg.addEventListener("mouseout", function () {
-      modalImg.style.scale = "1";
-    });
-
-    modalCloseButton.addEventListener("click", function () {
-      console.log("OK");
-      modal.remove();
-    });
-  });
-};
+  };
+  
