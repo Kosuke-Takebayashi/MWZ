47dcee4e86b8162cdf2250c9a367adebcf79f889
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-06-15 21:42:30 +0900

    拡大機能を追加

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-window.onload = function() {
-    // モーダルウィンドウの開閉をかく
-    // 特定のクラスを付与することでモーダルウィンドウとして機能させる
-    const targetImg = document.querySelector('.mwz');
-    let modalCloseButton;
+window.onload = function () {
+  // モーダルウィンドウの開閉をかく
+  // 特定のクラスを付与することでモーダルウィンドウとして機能させる
+  const targetImg = document.querySelector(".mwz");
+  let modalCloseButton;
+
+  targetImg.addEventListener("click", function () {
+    const modal = document.createElement("div");
+    const modalImgWrapper = document.createElement("div");
+    const modalImg = document.createElement("img");
+
+    modalCloseButton = document.createElement("button");
+    modal.className = "mwz-modal";
+    modalImgWrapper.className = "mwz-modal-img-wrapper";
+    modalImg.className = "mwz-modal-img";
+
+    modalCloseButton.className = "mwz-modal-close-button";
+    modalCloseButton.type = "button";
+    modalCloseButton.innerHTML = "閉じる";
     
-    targetImg.addEventListener('click', function() {
-        const modal = document.createElement('div');
-        const modalImg = document.createElement('img');
-        modalCloseButton = document.createElement('button');
-        modal.className = 'mwz-modal';
-        modalImg.className = 'mwz-modal-img';
-        modalCloseButton.className = 'mwz-modal-close-button';
-        modalCloseButton.type = 'button';
-        modalCloseButton.innerHTML = '閉じる';
-        modalImg.src = this.src;
-        modal.appendChild(modalImg);
-        modal.appendChild(modalCloseButton);
+    modalImg.src = this.src;
+    modal.appendChild(modalImgWrapper);
+    modalImgWrapper.appendChild(modalImg);
+    modal.appendChild(modalCloseButton);
 
-        // モーダルウィンドウを生成
-        document.body.insertBefore(modal, null);
+    // モーダルを生成
+    document.body.insertBefore(modal, null);
 
-        modalCloseButton.addEventListener('click', function() {
-        console.log('OK');
-        modal.remove();
-    })
-    })
+    modalImg.addEventListener("mouseover", function () {
+      modalImg.style.scale = "2";
+    });
 
-}
+    modalImg.addEventListener("mouseout", function () {
+      modalImg.style.scale = "1";
+    });
+
+    modalCloseButton.addEventListener("click", function () {
+      console.log("OK");
+      modal.remove();
+    });
+  });
+};
